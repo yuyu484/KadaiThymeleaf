b2adf446e13053bdd64e0733dbadef992cc547a0
mayu.watanabe <m_popo.xx@i.softbank.jp>
2025-01-05 14:41:20 +0900

    課題です。

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,21 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InputController {
+
+    @GetMapping("/input")
+    public String getInput(@RequestParam(name = "previous", required = false)String previous,Model model) {
+        // 入力画面を表示する際に、previousの値をモデルに追加
+        if (previous != null) {
+            model.addAttribute("previous", previous);
+        }
+        return "input"; // 入力画面(input.html)を返す
+    }
+}
+
+
---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,20 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutputController {
+
+    @PostMapping("/output")
+    public String postOutput(@RequestParam("val") String val, Model model) {
+        // フォームから送信されてきた値をModelに登録
+        model.addAttribute("val", val);
+        // output.htmlに画面遷移
+        return "output";
+}
+
+}
+
